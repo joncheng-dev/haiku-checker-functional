27cddd026d28d20375dc8e1d411c58cfdc413eb6
Jonathan Cheng <jcheng461@gmail.com>
2022-08-06 14:41:19 +0900

    lineCount function pass more tests

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -2,8 +2,16 @@ import { lineCheck } from "../src/haiku.js";
 
 // Check number of lines from user input
 describe("lineCheck function", () => {
-  const testingText = "How do you do?\nI am fine thank you.\nAnd you?";
+  const testingTextA = "How do you do?\nI am fine thank you.\nAnd you?";
+  const testingTextB = "Hi\nhello";
+  const testingTextC = "How are you?";
   test("should take text as an argument, and correctly return the number of new lines in the string", () => {
-    expect(lineCheck(testingText)).toEqual(3);
+    expect(lineCheck(testingTextA)).toEqual(3);
+  });
+  test("should take text as an argument, and correctly return the number of new lines in the string", () => {
+    expect(lineCheck(testingTextB)).toEqual(2);
+  });
+  test("should take text as an argument, and correctly return the number of new lines in the string", () => {
+    expect(lineCheck(testingTextC)).toEqual(1);
   });
 });
