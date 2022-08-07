2484fc62dca697ece9c04399c9ecaa4902215fdd
Jonathan Cheng <jcheng461@gmail.com>
2022-08-07 22:16:48 +0900

    pass tests for syllableCount function

--- __tests__/syllable-count.test.js
+++ __tests__/syllable-count.test.js
@@ -2,4 +2,48 @@ import syllableCount from "../src/syllable-count.js";
 
 describe("syllableCount function", () => {
   // To do
+  const wordOne = "eye";
+  const wordTwo = "hmm";
+  const wordThree = "label";
+  const wordFour = "table";
+  const wordFive = "stake";
+  const wordSix = "syllable";
+  const wordSeven = "teacher";
+  const wordEight = "four";
+  const wordNine = "heterozygous";
+  const wordTen = "thorough";
+  const wordEleven = "cyborg";
+  test("should take a string (word) as an argument, and correctly return the number of syllables it has", () => {
+    expect(syllableCount(wordOne)).toEqual(1);
+  });
+  test("should take a string (word) as an argument, and correctly return the number of syllables it has", () => {
+    expect(syllableCount(wordTwo)).toEqual(1);
+  });
+  test("should take a string (word) as an argument, and correctly return the number of syllables it has", () => {
+    expect(syllableCount(wordThree)).toEqual(2);
+  });
+  test("should take a string (word) as an argument, and correctly return the number of syllables it has", () => {
+    expect(syllableCount(wordFour)).toEqual(2);
+  });
+  test("should take a string (word) as an argument, and correctly return the number of syllables it has", () => {
+    expect(syllableCount(wordFive)).toEqual(1);
+  });
+  test("should take a string (word) as an argument, and correctly return the number of syllables it has", () => {
+    expect(syllableCount(wordSix)).toEqual(3);
+  });
+  test("should take a string (word) as an argument, and correctly return the number of syllables it has", () => {
+    expect(syllableCount(wordSeven)).toEqual(2);
+  });
+  test("should take a string (word) as an argument, and correctly return the number of syllables it has", () => {
+    expect(syllableCount(wordEight)).toEqual(1);
+  });
+  test("should take a string (word) as an argument, and correctly return the number of syllables it has", () => {
+    expect(syllableCount(wordNine)).toEqual(5);
+  });
+  test("should take a string (word) as an argument, and correctly return the number of syllables it has", () => {
+    expect(syllableCount(wordTen)).toEqual(2);
+  });
+  test("should take a string (word) as an argument, and correctly return the number of syllables it has", () => {
+    expect(syllableCount(wordEleven)).toEqual(2);
+  });
 });
--- src/syllable-count.js
+++ src/syllable-count.js
@@ -1,6 +1,5 @@
 const vowelCount = (word) => {
   const count = word.toLowerCase().match(/[aeiouy]/gi);
-  console.log(`Count is: ${count}`);
   if (count != null) {
     return count.length;
   } else {
@@ -10,21 +9,21 @@ const vowelCount = (word) => {
 
 const syllableCount = (word) => {
   let vowels = vowelCount(word);
-  if (word.length <= 3) {
+  if (word.length <= 3 && word.length > 0) {
     return 1;
-  } else if (word.length > 3) {
+  } else {
     // Subtract vowel if word ends with 'e' but not 'le'
     if (
       word.substr(word.length - 1) === "e" &&
       word.substr(word.length - 2) != "le"
     ) {
-      vowels = vowels - 1;
+      vowels -= 1;
     }
     // Subtract vowel if vowel follows another vowel.
     const regex = /[aeiouy](?=[aeiouy])/g;
     const found = word.match(regex);
     if (word.match(regex)) {
-      found.forEach((element) => (vowels -= 1));
+      found.forEach(() => (vowels -= 1));
     }
   }
   return vowels;
